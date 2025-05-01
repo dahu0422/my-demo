73c97c9b73250eb749206b6cbe2a439188fc44a6
Dahu-0712 <419460476@qq.com>
2025-05-01 14:40:34 +0800

    feat:update webpack-dev-server

---
+++ webpack/demos/08.webpack-dev-server/webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: 'development',
+  entry: {
+    main: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash:8].bundle.js',
+    // [name] will be replaced with the entry name, 
+    // [contenthash:8] will be replaced with a hash of the file content
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      chunks: ['main'],
+    }),
+  ],
+  devServer: {
+    hot: true,
+    port: 9000,
+  },
+}
