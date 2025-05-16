946d295fe91e9807296e0bae09da1c366f857ef8
dahu0422 <149865122+dahu0422@users.noreply.github.com>
2025-05-16 09:48:20 +0000

    feat:【umi-max】新建子应用

---
+++ umi-max/app1/src/app.ts
@@ -0,0 +1,28 @@
+// 运行时配置
+
+// 全局初始化数据配置，用于 Layout 用户信息和权限初始化
+// 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
+export async function getInitialState(): Promise<{ name: string }> {
+  return { name: '@umijs/max' };
+}
+
+export const layout = () => {
+  return {
+    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    menu: {
+      locale: false,
+    },
+  };
+};
+
+export const qiankun = {
+  async bootstrap() {
+    console.log('subapp bootstraped');
+  },
+  async mount(props: any) {
+    console.log('subapp mount', props);
+  },
+  async unmount() {
+    console.log('subapp unmount');
+  },
+};
