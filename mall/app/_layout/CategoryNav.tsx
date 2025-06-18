8867cb85931ca257334e37bc0fb4b37b46a87d25
zhangsongbo <zhangsongbo@4saving.com>
2025-06-18 15:43:48 +0800

    feat:【mall】初始化项目导航

---
+++ mall/app/_layout/CategoryNav.tsx
@@ -0,0 +1,10 @@
+// 商品类目导航
+export default function CategoryNav() { 
+  return (
+    <div className=" w-full h-11 border-b border-gray-200">
+      <div className="flex items-center max-w-(--container-xl) h-full mx-auto">
+        <h1>商品类目导航</h1>
+      </div>
+    </div>
+  )
+}
